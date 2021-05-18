ce360cb542347313fa1547c355f84951d543c435
Icaro Pinheiro <icaro@ipr.net.br>
2021-05-18 11:36:33 -0300

    Controller bancos serviço para buscar bancos febraban

---
+++ Controllers/BancosController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContaBancariaServer.Entities;
+using ContaBancariaServer.Interfaces;
+using ContaBancariaServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContaBancariaServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BancosController : ControllerBase
+    {
+        private IFebraban _febrabanService;
+
+        public BancosController (IFebraban febrabanService) {
+           _febrabanService = febrabanService;
+        }
+        // GET api/values
+        [HttpGet]
+        public febrabanLista Get()
+        {
+            return _febrabanService.getBancos();
+
+        }
+    }
+}
