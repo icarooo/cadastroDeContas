fedb998f4ddc2cb0f37f9b0dfb3a74f1cbba8eac
Icaro Pinheiro <icaro@ipr.net.br>
2021-05-18 11:36:14 -0300

    Controller Contas crud

---
+++ Controllers/ContasController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContaBancariaServer.Entities;
+using ContaBancariaServer.Interfaces;
+using ContaBancariaServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContaBancariaServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContasController : ControllerBase
+    {
+        private IContas _contaRepository;
+        private IFebraban _febrabanService;
+
+        public ContasController (IContas contaRepository, IFebraban febrabanService) {
+           _contaRepository = contaRepository;
+           _febrabanService = febrabanService;
+        }
+        // GET api/values
+        [HttpGet]
+        public List<conta> Get()
+        {
+            return _contaRepository.getContasList();
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public conta Get(int id)
+        {
+           return _contaRepository.getContaById(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public Task<conta> Post([FromBody] conta conta)
+        {
+            return _contaRepository.gravarDados(conta);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public Task<conta> Put(int id, [FromBody] conta conta)
+        {
+            conta.id = id;
+            return _contaRepository.gravarDados(conta);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
