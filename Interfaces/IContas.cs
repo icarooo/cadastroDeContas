a739082940c330ab440850883724f599034df309
Icaro Pinheiro <icaro@ipr.net.br>
2021-05-18 11:33:55 -0300

    Interface contas

---
+++ Interfaces/IContas.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ContaBancariaServer.Models;
+
+namespace ContaBancariaServer.Interfaces
+{
+    public interface IContas
+    {
+         List<conta> getContasList();
+         conta getContaById(int id);
+         Task<conta> gravarDados(conta conta);
+
+    }
+}
