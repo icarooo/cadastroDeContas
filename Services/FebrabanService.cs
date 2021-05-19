fade5fcff12c464441d39f8e4047f680ca0b922d
Icaro Pinheiro <icaro@ipr.net.br>
2021-05-19 00:09:52 -0300

    Omitindo bancos sem codigo

--- Services/FebrabanService.cs
+++ Services/FebrabanService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using ContaBancariaServer.Interfaces;
 using ContaBancariaServer.Models;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace ContaBancariaServer.Services
 {
@@ -16,8 +17,9 @@ namespace ContaBancariaServer.Services
 
             var content = new StringContent("{	FiltroAssociado: \"Todos\" }", Encoding.UTF8, "application/json");
             var result = _httpClient.PostAsync("https://portal.febraban.org.br/Associado/Index",content).Result.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<febrabanLista>(result.Result);
+            febrabanLista lista= JsonConvert.DeserializeObject<febrabanLista>(result.Result);
+            lista.listaBancos = lista.listaBancos.Where(c => c.Compensacao != null && c.Compensacao != " " && c.Compensacao != String.Empty).ToList();
+            return lista;
         }
     }
 }
